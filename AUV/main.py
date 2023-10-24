b8bcf3cbfd4d895872dc4cc153f5b8a12e087bbd
Lees <msle237.lees@gmail.com>
2023-10-24 18:09:27 -0400

    fixed the map function

--- AUV/main.py
+++ AUV/main.py
@@ -82,7 +82,7 @@ for event in pygame.event.get():
 data = np.zeros(7)
 
 def map_range(x : float, in_min : float, in_max : float, out_min : float, out_max : float) -> float:
-    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 # Main loop
 while True:
