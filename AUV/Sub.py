5fadc277678657c510fadf31a5f6f9455addd18b
Lees <msle237.lees@gmail.com>
2023-11-01 17:40:08 -0400

    sub.py wasn't added

---
+++ AUV/Sub.py
@@ -0,0 +1,6 @@
+from modules.Movement_Package import Movement_Package
+from modules.Networking_Package import Networking_Package
+from modules.Hardware_Interface import Hardware_Interface
+from modules.Regular_Camera_Package import Regular_Camera_Package
+from modules.Zed_Camera_Package import Zed_Camera_Package
+
